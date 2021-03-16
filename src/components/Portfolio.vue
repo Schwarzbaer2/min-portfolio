1eda6933ade60d8fad43e45e622ba40c52a93754
Andreas Schwarz <andreas.schwarz20@posteo.de>
2021-03-16 11:45:34 +0100

    Fixing Portfolio Layout

--- src/components/Portfolio.vue
+++ src/components/Portfolio.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="px-8 pt-24">
+  <div class="px-8 pt-10 md:px-10 md:pt-16 xl:pl-40 xl:pr-56">
     <PortfolioCard
       v-for="project in projects"
       :key="project.id"
--- src/components/PortfolioCard.vue
+++ src/components/PortfolioCard.vue
@@ -1,21 +1,37 @@
 <template>
-  <div class="flex justify-center text-grayBlue">
-    <img :src="project.mobileImg" />
+<section  class="flex flex-col md:flex-row md:items-center md:px-10 xl:mb-20">
+  <div class="flex text-grayBlue ">
+    <div class="md:hidden">
+    <img  :src="project.mobileImg" />
+    </div>
+    <div class="hidden pr-16 md:block xl:hidden ">
+    <img :src="project.tabletImg" />
+    </div>
+    <div class="hidden min-w-full pr-32 xl:block">
+    <img  :src="project.desktopImg" />
+    </div>
   </div>
 
-  <div class="pt-14">
+
+  <div class="pb-16 border-b pt-14 md:pt-0 md:pb-0 xl:py-24 xl:w-4/6 ">
     <div
       class="text-4xl font-bold leading-loose tracking-tight text-center font-ibarra"
     >
-      <h2>{{ project.title }}</h2>
+      <h2 class="py-6 border-t">{{ project.title }}</h2>
     </div>
     <div class="text-base leading-8 font-publicSans">
       {{ project.description }}
     </div>
+    <div class="py-6"> 
+    <base-button buttonType="secondary">View Project</base-button>
+    </div>
   </div>
+</section>
 </template>
 
 <script>
+import BaseButton from './BaseButton.vue'
+
 export default {
   name: 'PortfolioCard',
   props: {
@@ -23,7 +39,9 @@ export default {
       type: Object,
       required: true
     }
-  }
+ 
+  },
+   components: { BaseButton }
 }
 </script>
 
