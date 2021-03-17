8ae2104eb0a412c819e4bc2dd40a4b12abb7bfb4
Andreas Schwarz <andreas.schwarz20@posteo.de>
2021-03-17 12:47:43 +0100

    Fixing Portfolio Images

--- src/components/PortfolioCard.vue
+++ src/components/PortfolioCard.vue
@@ -2,25 +2,39 @@
 <section  class="flex flex-col md:flex-row md:items-center md:px-10 xl:mb-20">
   <div class="flex text-grayBlue"  
 
-   :class="{'md:order-last': project.id % 2 != 1}"
-
-
- >
+   :class="{'md:order-last': project.id % 2 != 1}">
 
     <div class="md:hidden">
     <img  :src="project.mobileImg" />
     </div>
-    <div class="hidden pr-16 md:block xl:hidden"
-         :class="{'pr-0 pl-16': project.id % 2 != 1}"
+    <div class="hidden md:block xl:hidden"
+       :class="[
+      {
+        'pr-16': project.id % 2 != 0
+      },
+      {
+       'pl-16': project.id % 2 != 1
+      }
+    ]"
+         
     >
     <img :src="project.tabletImg" />
     </div>
     <div class="hidden min-w-full pr-32 xl:block"
-     :class="{'pr-0 pl-32': project.id % 2 != 1}"
+            :class="[
+      {
+        'pr-32': project.id % 2 != 0
+      },
+      {
+       'pl-32': project.id % 2 != 1
+      }
+    ]"
     >
     <img  :src="project.desktopImg" />
     </div>
   </div>
+
+
 
 
   <div class="pb-16 border-b pt-14 md:pt-0 md:pb-0 xl:py-32 xl:w-4/6 ">
